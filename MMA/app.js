cf054d42dc2197bc5dfe5240fa05d42a0357ad15
Matthew Butt <matthew@matthewbutt.com>
2012-06-14 11:58:23 +0100

    Replace player squares with dots

--- MMA/app.js
+++ MMA/app.js
@@ -3,6 +3,7 @@ var request = require("request")
 
 ,	input = process.argv[5]
 ,	player = input.substr(0,1)
+,	opponent = player === "1" ? "2" : "1"
 ,	board = input.split("\\n")
 ,	x, y
 ,	boardLength = board.length
@@ -11,18 +12,23 @@ var request = require("request")
 ,	boardArray = []
 ,	value
 ,	playerCoordinate
+,	opponentCoordinate
 ,	finishCoordinate;
 
-for(y = 1; y < boardLength; y = y + 1) {
-	row = board[y];
-	rowLength = board[y].length;
+for(y = 0; y < boardLength - 1; y = y + 1) {
+	row = board[y + 1];
+	rowLength = row.length;
 	for(x = 0; x < rowLength; x = x + 1) {
 		if (!boardArray[x]) boardArray[x] = [];
 		value = row[x];
-		boardArray[x][y - 1] = value;
+		boardArray[x][y] = value;
 		
 		if(value === player) {
 			playerCoordinate = [x,y];
+			boardArray[x][y] = ".";
+		} else if (value === opponent) {
+			opponentCoordinate = [x,y];
+			boardArray[x][y] = ".";
 		} else if (value === "F") {
 			finishCoordinate = [x,y];
 		}
@@ -30,3 +36,6 @@ for(y = 1; y < boardLength; y = y + 1) {
 }
 
 console.log(boardArray);
+console.log("Player: " + playerCoordinate);
+console.log("Opponent: " + opponentCoordinate);
+console.log("Finish: " + finishCoordinate);
