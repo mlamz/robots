90d31e88849d108821bbec782f333737268f0005
Michael Lam <michael@michaellam.co.uk>
2012-06-16 15:25:00 +0100

    remove original implementation. start again with simple test

--- MMA/spec/maze_spec.js
+++ MMA/spec/maze_spec.js
@@ -2,12 +2,29 @@ var should = require('chai').should()
 , child_process =	require('child_process')
 , path = require("path")
 ,	process_path = path.join(__dirname, "../app.js")
-, maze_string = '1\\n*******\\n*.1.***\\n*...2**\\n**F...*\\n*******'
-, fs = require('fs');
+, maze_string = ''
+,	fs = require('fs');
 
 describe("maze", function(){
 
-	it("should return character for direction", function(done){
+	/*given a maze of 	You are player 1
+						***
+						*1*
+						*.*
+						*F*
+						*2*
+	*/
+	it("should move south", function(done){
+		getOutput(
+			"You are player 1\n***\n*1*\n*.*\n*F*\n*2*",
+			function(result){
+				result.should.equal("S\n");
+				done();
+			});
+	});
+
+	function getOutput(maze, callback){
+		maze_string = "You are player 1\n***\n*1*\n*.*\n*F*\n*2*";
 		var output = '', program = child_process.spawn("node", [process_path, maze_string]);
 
 		program.stdout.on('data', function(data){
@@ -15,28 +32,12 @@ describe("maze", function(){
 		});
 
 		program.stdout.on('end', function(data){
-			output.should.equal('S\n');
-			done();
+			callback(output);
+
 		});
 
 		program.stderr.on('data', function(data){
 			console.log("STDERR", data.toString());
 		});
-	});
+	}
 });
-
-/*
-maze string is:
-
-1
-*******
-*.1.***
-*...2**
-**F...*
-*******
-
-command to run is:
-node app.js "1\\n*******\\n*.1.***\\n*...2**\\n**F...*\\n*******"
-
-*/
-
--- app.js
+++
@@ -1,8 +0,0 @@
-var request = require("request")
-,	underscore = require("underscore")
-,	wait = true;
-
-console.log("length", process.argv.length);
-_.each(process.argv, function(arg){
-	console.log(arg);
-});
