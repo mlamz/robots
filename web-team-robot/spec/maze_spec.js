f918eae0f16c1a3ae541f17a14fe618ec07e1c98
Michael Lam <treadsafely@gmail.com>
2012-06-20 11:48:55 +0100

    framework for web team js tdd sessions

---
+++ web-team-robot/spec/maze_spec.js
@@ -0,0 +1,59 @@
+var should = require('chai').should()
+,	getOutput = require('./getOutput');
+
+describe("maze", function(){
+
+	/*given a maze of 	You are player 1
+						***
+						*1*
+						*.*
+	*/
+	it("should move south", function(done){
+		getOutput(
+			"You are player 1\n'***'\n'*1*'\n'*.*'",
+			function(result){
+				result.should.equal("S\n");
+				done();
+			});
+	});
+
+	/*given a maze of 	You are player 1
+						**
+						*1.
+						**
+	*/
+	it("should move east", function(done){
+		getOutput(
+			"You are player 1\n'**'\n'*1.'\n'**'\n",
+			function(result){
+				result.should.equal("E\n");
+				done();
+			});
+	});
+
+	/*given a maze of 	You are player 1
+						***
+						*.1
+	*/
+	it("should move west", function(done){
+		getOutput(
+			"You are player 1\n'***'\n'*.1'",
+			function(result){
+				result.should.equal("W\n");
+				done();
+			});
+	});
+
+	/*given a maze of 	You are player 1
+						*.*
+						*1*
+	*/
+	it("should move north", function(done){
+		getOutput(
+			"You are player 1\n'*.*'\n'*1*'",
+			function(result){
+				result.should.equal("N\n");
+				done();
+			});
+	});
+});
